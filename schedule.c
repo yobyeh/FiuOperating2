523ab63ecd100de0a90f9a908ceadde579d3106d
JT <yobyeh@gmail.com>
2024-02-24 22:28:34 -0500

    cleanup and order print

--- schedule.c
+++ schedule.c
@@ -28,17 +28,10 @@ int main(int argc, char **argv)
         fscanf(file, "%d", arrival + i);
         fscanf(file, "%d", burst + i);
     }
-    // You should close the file after reading, even though it's usually ok to forget
+    //Close the file after reading
     fclose(file);
-    
-    // In this example I only print the values, in your assignment you have to compute the execution sequence
-    // as well as the average waiting time and average turnaround time
-    printf("There are %d processes\n", numProcess);
-    for (int i = 0; i < numProcess; i++){
-        printf("Process %d arrives at %d with burst time %d\n", i + 1, arrival[i], burst[i]);
-    }
 
-    //identifying scedule selection or exit
+    //Identifying scedule selection or exit
     int userSelect = 0;
     if(strcmp(argv[2], "FCFS") == 0){
         userSelect = 1;
@@ -51,20 +44,21 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    //tracking process labels
+    //Tracking process labels
     int processLables[numProcess];
     for(int i = 0; i < numProcess; i++){
         processLables[i] = i + 1;
     }
-    //sorting arrays by arrival time
+
+    //Sorting arrays by arrival time
     for(int i = 0; i < numProcess; i++){
         int oldArrival = arrival[i];
         int oldBurst = burst[i];
-    int oldLable = processLables[i];
+        int oldLable = processLables[i];
         for(int j = i + 1; j < numProcess; j++){
             int newArrival = arrival[j];
             int newBurst = burst[j];
-    int newLable = processLables[j];
+            int newLable = processLables[j];
             if(oldArrival > newArrival){
                 arrival[i] = newArrival;
                 arrival[j] = oldArrival;
@@ -76,35 +70,31 @@ int main(int argc, char **argv)
         }
     }
 
+    //Tracking start and end time of process
     int startTime[numProcess];
     int endTime[numProcess];
-
     for(int i = 0; i < numProcess; i++) {
     startTime[i] = -1;
     endTime[i] = -1;
     }
 
-    //run desired schedule
-    //output order Average wait and turnaround
+    //Run desired schedule
     switch (userSelect)
     {
         //FCFS
         case 1:{
             
-            //print process order
+            //Print process order
             printf("Process order\n");
             for (int i = 0; i < numProcess; i++){
                 printf(" %d ", processLables[i]);
             }
             printf("\n");
 
-            //running FCFS
+            //Running FCFS
             int currentTime = 0;
             int currentProcess = 0;
             int completedProcess = 0;
-            //int startTime[numProcess];
-            //int endTime[numProcess];
-
             while(completedProcess < numProcess){
                 if(arrival[currentProcess] <= currentTime){
                     startTime[currentProcess] = currentTime;
@@ -117,7 +107,7 @@ int main(int argc, char **argv)
                 }
             }
             
-            //average wait
+            //Average wait
             int totalWait = 0;
             for (int i = 0; i < numProcess; i++){
                 int waitTime = startTime[i] - arrival[i];
@@ -126,7 +116,7 @@ int main(int argc, char **argv)
             double averageWait = (double)(totalWait) / numProcess;
             printf("average wait time is: %f\n", averageWait);
 
-            //average turnaround
+            //Average turnaround
             int totalTurn = 0;
             for (int i = 0; i < numProcess; i++){
                 int turn = endTime[i] - arrival[i];
@@ -134,7 +124,6 @@ int main(int argc, char **argv)
             }
             double averageTurn = (double)(totalTurn) / numProcess;
             printf("Average turnaround time is: %f\n", averageTurn);
-
             break;
         }
         //SJF
@@ -142,13 +131,13 @@ int main(int argc, char **argv)
             int completedProcess = 0;
             int currentTime = 0;
             int visited[numProcess];
-
+            int orderCompleted[numProcess];
             for(int i = 0; i < numProcess; i++){
                 visited[i] = 0;
             }
 
+            //Running SJF
             while(completedProcess < numProcess){
-
                 int shortBurstLocation = 0;
                 int shortBurst = INT_MAX;
                 for(int i = 0; i < numProcess; i++){
@@ -157,19 +146,24 @@ int main(int argc, char **argv)
                     shortBurstLocation = i;
                     }
                 }
-                
-                
                 if(startTime[shortBurstLocation] == -1){
                     startTime[shortBurstLocation] = currentTime;
                 }
-
                 currentTime += shortBurst; // Process completes
                 endTime[shortBurstLocation] = currentTime;
                 visited[shortBurstLocation] = 1;
+                orderCompleted[completedProcess] = processLables[shortBurstLocation];
                 completedProcess++;
             }
 
-             //average wait
+            //Print order
+            printf("Process order\n");
+            for(int i = 0; i < numProcess; i++){
+                printf(" %d ", orderCompleted[i]);
+            }
+
+
+            //Average wait
             int totalWait = 0;
             for (int i = 0; i < numProcess; i++){
                 int waitTime = startTime[i] - arrival[i];
@@ -178,7 +172,7 @@ int main(int argc, char **argv)
             double averageWait = (double)(totalWait) / numProcess;
             printf("average wait time is: %f\n", averageWait);
 
-            //average turnaround
+            //Average turnaround
             int totalTurn = 0;
             for (int i = 0; i < numProcess; i++){
                 int turn = endTime[i] - arrival[i];
@@ -186,14 +180,13 @@ int main(int argc, char **argv)
             }
             double averageTurn = (double)(totalTurn) / numProcess;
             printf("Average turnaround time is: %f\n", averageTurn);
-
             break;
         }
         default:
             break;
     }
     
-    // And remember to release the dynamically allocated memory after using
+    //Release the dynamically allocated memory
     free(arrival);
     free(burst);
 
