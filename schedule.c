b43bc4b6e454b58799cc889175acbb06d425bb25
JT <yobyeh@gmail.com>
2024-02-24 20:15:59 -0500

    1

--- schedule.c
+++ schedule.c
@@ -107,6 +107,7 @@ int main(int argc, char **argv)
                     currentTime += burst[currentProcess];
                     endTime[currentProcess] = currentTime;
                     completedProcess++;
+                    currentProcess++;
                 }else{
                     currentTime++;
                 }
